09d144c9c81914a62b42565e7afea823cb0cbff9
KenichiNishiya <yorimitsu@proton.me>
2024-05-02 19:46:32 -0300

    Update

--- app/src/main/java/com/example/glem/HomeFragment.kt
+++ app/src/main/java/com/example/glem/HomeFragment.kt
@@ -1,10 +1,25 @@
 package com.example.glem
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.File
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +33,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
+    private lateinit var text_show: TextView
     private var param1: String? = null
     private var param2: String? = null
 
@@ -34,8 +50,137 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // Inicializando elementos View
+        val marca = rootView.findViewById<EditText>(R.id.inputMarca)
+        val modelo = rootView.findViewById<EditText>(R.id.inputModelo)
+        val ano = rootView.findViewById<EditText>(R.id.inputAno)
+        val imageView = rootView.findViewById<ImageView>(R.id.imageView)
+
+        var text_show = rootView.findViewById<TextView>(R.id.show_text)
+
+        // Criando a conexao com o bd do Firebase
+        val storage = Firebase.storage("gs://glem-android.appspot.com")
+        val storageRef = storage.reference
+        val imageRef = storageRef.child("images/AE86.jpg")
+        val localFile = File.createTempFile("tempImage", "jpg")
+
+        imageRef.getFile(localFile)
+            .addOnSuccessListener {
+                // Se conseguir baixar a imagem, transforma em bitmap, dimensiona e seta na view
+                val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+                val scaledBitmap = Bitmap.createScaledBitmap(bitmap, 700, 500, true)
+                imageView.setImageBitmap(scaledBitmap)
+            }
+            .addOnFailureListener {
+                // Executa se acontecer algum erro
+                Toast.makeText(activity, "Erro ao carregar imagem", Toast.LENGTH_SHORT).show()
+            }
+
+        val add_button: Button = rootView.findViewById(R.id.button_add)
+        add_button.setOnClickListener {
+            // Pega o input do user
+            val marcaText = marca.text.toString()
+            val modeloText = modelo.text.toString()
+            val anoText = ano.text.toString()
+
+            // Insere no bd se os campos estiverem preenchidos
+            if(marcaText.isNotBlank() && modeloText.isNotBlank() && anoText.isNotBlank()) {
+                addDataToFirestore(marcaText, modeloText, anoText)
+
+                marca.setText("")
+                modelo.setText("")
+                ano.setText("")
+            }
+            else{
+                Toast.makeText(activity, "Digite todos os dados", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        val show_button: Button = rootView.findViewById(R.id.button_show)
+        show_button.setOnClickListener {
+            retrieveDataFromFirestore()
+        }
+
+        val buttonFragment: Button = rootView.findViewById(R.id.button_fragment)
+
+        buttonFragment.setOnClickListener {
+            // Create a FragmentManager
+            val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+
+            // Begin a FragmentTransaction
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+            // Replace the contents of the container with the new fragment
+            val fragment = ScrollingFragment()
+            fragmentTransaction.replace(R.id.fragment_container, fragment)
+
+            // Add the transaction to the back stack
+            fragmentTransaction.addToBackStack(null)
+
+            // Commit the transaction
+            fragmentTransaction.commit()
+        }
+
+        return rootView
     }
+
+    fun addDataToFirestore(marca: String, modelo: String, ano: String) {
+
+        // Cria uma referencia ao Firestore
+        val db = FirebaseFirestore.getInstance()
+
+        // Cria um novo documento na collection de carros
+        val carrosCollection = db.collection("carros")
+        val newCarDoc = carrosCollection.document()
+
+        // Adiciona dados no documento
+        val data = hashMapOf(
+            "marca" to marca,
+            "modelo" to modelo,
+            "ano" to ano
+        )
+
+        newCarDoc.set(data)
+            .addOnSuccessListener {
+                Toast.makeText(activity, "Inserido com sucesso", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(activity, "Erro ao inserir o documento", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    fun retrieveDataFromFirestore() {
+
+        // Cria uma referecia ao Firestore > collection carros
+        val db = FirebaseFirestore.getInstance()
+        val carrosCollection = db.collection("carros")
+
+        // Faz um get para pegar todos os dados da collection
+        carrosCollection.get()
+            .addOnSuccessListener { result ->
+                // Cria uma string pra colocar todos os itens
+                val dataStringBuilder = StringBuilder()
+
+                // Loop para iterar cada elemento da collection
+                for (document in result) {
+                    val marca = document.getString("marca")
+                    val modelo = document.getString("modelo")
+                    val ano = document.getString("ano")
+
+                    dataStringBuilder.append("Marca: $marca, Modelo: $modelo, Ano: $ano\n")
+                }
+
+                // Atualiza a textview
+                val allData = dataStringBuilder.toString()
+                text_show.text = allData
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(activity, "Erro ao carregar dados", Toast.LENGTH_SHORT).show()
+            }
+    }
+
 
     companion object {
         /**
--- app/src/main/java/com/example/glem/MainActivity.kt
+++ app/src/main/java/com/example/glem/MainActivity.kt
@@ -26,8 +26,6 @@ import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.ktx.storage
 import java.io.File
 
-
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,76 +39,6 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-
-        // Inicializando elementos View
-        val marca = findViewById<EditText>(R.id.inputMarca)
-        val modelo = findViewById<EditText>(R.id.inputModelo)
-        val ano = findViewById<EditText>(R.id.inputAno)
-
-        val imageView = findViewById<ImageView>(R.id.imageView)
-
-        // Criando a conexao com o bd do Firebase
-        val storage = Firebase.storage("gs://glem-android.appspot.com")
-        var storageRef = storage.reference
-        var imageRef = storageRef.child("images/AE86.jpg")
-        val localFile = File.createTempFile("tempImage", "jpg")
-
-        imageRef.getFile(localFile)
-            .addOnSuccessListener {
-                // Se conseguir baixar a imagem, transforma em bitmap, dimensiona e seta na view
-                val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
-                val scaledBitmap = Bitmap.createScaledBitmap(bitmap, 700, 500, true)
-                imageView.setImageBitmap(scaledBitmap)
-            }
-            .addOnFailureListener {
-                // Executa se acontecer algum erro
-                Toast.makeText(applicationContext, "Erro ao carregar imagem", Toast.LENGTH_SHORT).show()
-            }
-
-        val add_button: Button = findViewById(R.id.button_add)
-        add_button.setOnClickListener {
-            // Pega o input do user
-            var marcaText = marca.text.toString()
-            var modeloText = modelo.text.toString()
-            var anoText = ano.text.toString()
-
-            // Insere no bd se os campos estiverem preenchidos
-            if(marcaText != "" && modeloText != "" && anoText != "") {
-                addDataToFirestore(marcaText, modeloText, anoText)
-
-                marca.setText("")
-                modelo.setText("")
-                ano.setText("")
-            }
-            else{
-                Toast.makeText(applicationContext, "Digite todos os dados", Toast.LENGTH_LONG).show()
-            }
-        }
-
-        val show_button: Button = findViewById(R.id.button_show)
-        show_button.setOnClickListener {
-            retrieveDataFromFirestore()
-        }
-
-        val buttonFragment: Button = findViewById(R.id.button_fragment)
-
-        buttonFragment.setOnClickListener {
-            // Create a FragmentManager
-            val fragmentManager: FragmentManager = supportFragmentManager
-
-            // Begin a FragmentTransaction
-            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-
-            // Replace the contents of the container with the new fragment
-            val fragment = ScrollingFragment()
-            fragmentTransaction.replace(R.id.fragment_container, fragment)
-
-            // Add the transaction to the back stack
-            fragmentTransaction.addToBackStack(null)
-
-            // Commit the transaction
-            fragmentTransaction.commit()
-        }
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
@@ -124,7 +52,7 @@ class MainActivity : AppCompatActivity() {
             val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
 
             // Replace the contents of the container with the new fragment
-            val fragment = ScrollingFragment()
+            val fragment = HomeFragment()
             fragmentTransaction.replace(R.id.fragment_container, fragment)
 
             // Add the transaction to the back stack
@@ -137,6 +65,20 @@ class MainActivity : AppCompatActivity() {
             true
         }
         R.id.func2 -> {
+            val fragmentManager: FragmentManager = supportFragmentManager
+
+            // Begin a FragmentTransaction
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+            // Replace the contents of the container with the new fragment
+            val fragment = ScrollingFragment()
+            fragmentTransaction.replace(R.id.fragment_container, fragment)
+
+            // Add the transaction to the back stack
+            fragmentTransaction.addToBackStack(null)
+
+            // Commit the transaction
+            fragmentTransaction.commit()
             true
         }
         else -> {
@@ -144,59 +86,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun addDataToFirestore(marca: String, modelo: String, ano: String) {
-
-        // Cria uma referencia ao Firestore
-        val db = FirebaseFirestore.getInstance()
-
-        // Cria um novo documento na collection de carros
-        val carrosCollection = db.collection("carros")
-        val newCarDoc = carrosCollection.document()
-
-        // Adiciona dados no documento
-        val data = hashMapOf(
-            "marca" to marca,
-            "modelo" to modelo,
-            "ano" to ano
-        )
-
-         newCarDoc.set(data)
-             .addOnSuccessListener {
-                 Toast.makeText(applicationContext, "Inserido com sucesso", Toast.LENGTH_SHORT).show()
-             }
-             .addOnFailureListener { e ->
-                 Toast.makeText(applicationContext, "Erro ao inserir o documento", Toast.LENGTH_SHORT).show()
-             }
-    }
-
-    fun retrieveDataFromFirestore() {
-        var text_show: TextView = findViewById(R.id.show_text)
-
-        // Cria uma referecia ao Firestore > collection carros
-        val db = FirebaseFirestore.getInstance()
-        val carrosCollection = db.collection("carros")
-
-        // Faz um get para pegar todos os dados da collection
-        carrosCollection.get()
-            .addOnSuccessListener { result ->
-                // Cria uma string pra colocar todos os itens
-                val dataStringBuilder = StringBuilder()
-
-                // Loop para iterar cada elemento da collection
-                for (document in result) {
-                    val marca = document.getString("marca")
-                    val modelo = document.getString("modelo")
-                    val ano = document.getString("ano")
-
-                    dataStringBuilder.append("Marca: $marca, Modelo: $modelo, Ano: $ano\n")
-                }
-
-                // Atualiza a textview
-                val allData = dataStringBuilder.toString()
-                text_show.text = allData
-            }
-            .addOnFailureListener { exception ->
-                Toast.makeText(applicationContext, "Erro ao carregar dados", Toast.LENGTH_SHORT).show()
-            }
-    }
 }
